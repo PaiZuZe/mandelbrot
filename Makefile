4906c4af7fb7676f7eb3ccc79db0932117fc45a7
PaiZuZe <vitinho_cga@hotmail.com>
2019-05-07 20:18:25 -0300

    I'm a markov chain, as in fuck me dead if i remember what i did.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS=-Wall -Wextra -Werror -pedantic -lpthread
+CFLAGS=-Wall -Wextra -Werror -pedantic -lpthread `libpng-config --ldflags`
 
 file: mandelbrot.cpp
 	g++ mandelbrot.cpp $(CFLAGS) -g -O2 -o mbrot
@@ -6,6 +6,6 @@ file: mandelbrot.cpp
 blob:
 	g++ mandelbrot.cpp -O2 -o mbrot
 test:
-	./mbrot 1.7 4.5 2.3 5.4 100 100 CPU 4 bob.png
+	./mbrot 0.0 0.0 -0.2 -0.2 5 5 CPU 4 bob.png
 clean:
-	rm mandelbrot
+	rm mbrot
--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -3,6 +3,8 @@
 #include <string> //stof
 #include <vector>
 
+#include <png++/png.hpp>
+
 #define INTER_LIMIT 200
 
 #define DIE(...) { \
@@ -13,28 +15,31 @@
 int get_inter(std::complex<float> c) {
     std::complex<float> z(0.0, 0.0);
     int i;
-    for (i = 0; i < INTER_LIMIT; i++) {
-        if (std::abs(z) > 2) {
-            break;
-        }
+    for (i = 0; i < INTER_LIMIT || std::norm(z) > 4; i++) {
         z = std::pow(z, 2) + c;
     }
     return i;
 }
 
-std::vector<std::vector<int>> sinlge_mandel(std::complex<float> c0, std::complex<float> c1, const int del_y, const int del_x, const int w, const int h){
-    std::vector<std::vector<int>> res_matrix;
-    std::complex<float> del(del_x, del_y);
-    for (int i = 0; i < w; i += del_x) {
-        res_matrix.push_back(std::vector<int> ());
-        for (int j = 0; j < h; j += del_y) {
+std::vector<std::vector<int>> sinlge_mandel(std::complex<float> c0, const float del_y, const float del_x, const int w, const int h){
+    std::vector<std::vector<int>> res_matrix(w + 1, std::vector<int>(h + 1));
+    std::complex<float> del(0, 0);
+    for (int i = 0; i <= h; i += 1) {
+        for (int j = 0; j <= w; j += 1) {
+            res_matrix[i][j] = get_inter(c0 + del);
             del.imag(del.imag() + del_y);
-            res_matrix[i].push_back(get_inter(c0 + del));
-
         }
         del.real(del.real() + del_x);
-        del.imag(del_y);
+        del.imag(0);
     }
+
+    for (int i = 0; i < h; i += 1) {
+        for (int j = 0; j < w; j += 1) {
+            std::cout << res_matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return res_matrix;
 }
 
@@ -46,13 +51,14 @@ int main(int argc, char** argv) {
     std::complex<float> c1(std::stof(argv[3]), std::stof(argv[4]));
     const int w = std::stoi(argv[5]);
     const int h = std::stoi(argv[6]);
-    const int del_x = (w * 0.01 > 1) ? w * 0.01 : 1;  //WRONG ?????
-    const int del_y = (h * 0.01 > 1) ? h * 0.01 : 1;  //WRONG ?????
+    const float del_x = (c1.real() - c0.real()) / w;
+    const float del_y = (c1.imag() - c0.imag()) / h;
+    /*
     const int num_threads = std::stoi(argv[8]);
     const std::string comp_flag = argv[7];
     const std::string file_name = argv[9];
-
-    sinlge_mandel(c0, c1, del_y, del_x, w, h);
+    */
+    sinlge_mandel(c0, del_y, del_x, w, h);
 
     return 0;
 }
