407ad656708e5de11906f59084396c557e79c78d
PaiZuZe <vitinho_cga@hotmail.com>
2019-05-11 18:45:40 -0300

    Fix things

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -38,13 +38,12 @@ int get_inter(std::complex<float> c) {
 
 void sinlge_mandel(std::vector<std::vector<int>> &res_matrix, std::complex<float> c0, const float del_y, const float del_x, const int w, const int h){
     std::complex<float> del(0, 0);
-    for (int i = 0; i <= h; i += 1) {
-        for (int j = 0; j <= w; j += 1) {
+    for (int i = 0; i < h; i += 1) {
+        for (int j = 0; j < w; j += 1) {
+            del.real(del_x * j);
+            del.imag(del_y * i);
             res_matrix[i][j] = get_inter(c0 + del);
-            del.imag(del.imag() + del_y);
         }
-        del.real(del.real() + del_x);
-        del.imag(0);
     }
     return;
 }
@@ -56,7 +55,6 @@ void create_picture(std::vector<std::vector<int>> &matrix, const std::string fil
             image[i][j] = png::rgb_pixel(matrix[i][j], matrix[i][j], matrix[i][j]);
         }
     }
-    print(matrix);
     image.write(file_name);
 
 }
@@ -69,17 +67,17 @@ int main(int argc, char** argv) {
     std::complex<float> c1(std::stof(argv[3]), std::stof(argv[4]));
     const int w = std::stoi(argv[5]);
     const int h = std::stoi(argv[6]);
-    const float del_x = (c1.real() - c0.real()) / w;
-    const float del_y = (c1.imag() - c0.imag()) / h;
+    const float del_x = (c1.real() - c0.real()) / (w - 1);
+    const float del_y = (c1.imag() - c0.imag()) / (h - 1);
     /*
     const int num_threads = std::stoi(argv[8]);
     const std::string comp_flag = argv[7];
     */
     const std::string file_name = argv[9];
     std::vector<std::vector<int>> res;
-    res.resize(h + 1);
-    for (int i = 0; i < h + 1; ++i) {
-        res[i].resize(w + 1);
+    res.resize(h);
+    for (int i = 0; i < h; ++i) {
+        res[i].resize(w);
     }
     sinlge_mandel(res, c0, del_y, del_x, w, h);
     create_picture(res, file_name, w, h);
