c963f2cbd9a4a9d52f88ad2666b41ee2685a20fd
PaiZuZe <victorcga96@gmail.com>
2019-05-20 16:17:47 -0300

    More CUDA that does not work :)

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 CFLAGS=-Wall -Wextra -pedantic -fopenmp -lpthread `libpng-config --ldflags`
+NVCC=nvcc
+CC=g++
 
-file: mandelbrot.cpp
-	g++ mandelbrot.cpp $(CFLAGS) -g -O2 -o mbrot
+mandel.o: mandel.cu
+	$(NVCC) mandel.cu
 
-blob:
-	g++ mandelbrot.cpp -O2 -o mbrot
+all: mandel.o
+	$(CC) mandelbrot.cpp mandel.o $(CFLAGS) -g -O2 -o mbrot
+
+.PHONY: test
 test:
 	./mbrot -2.0 -2.0 2.9 2.0 1920 1080 CPU 4 bob.png
+.PHONY: clean
 clean:
 	rm mbrot
 	rm bob.png
--- mandel.cu
+++ mandel.cu
@@ -1,5 +1,9 @@
-__device__ int get_interger (thrust::complex<float> c) {
+#include "mandel.cuh"
+
+__device__ int get_inter (thrust::complex<float> c) {
     int i;
+    thrust::complex<float> z(0.0, 0.0);
+
     for (i = 0; i< INTER_LIMIT; ++i) {
         if (thrust::norm(z) > 4 ) {
             break;
@@ -9,27 +13,36 @@ __device__ int get_interger (thrust::complex<float> c) {
     return i;
 }
 
-__global__ void fill_matrix (thrust::device_vector<float> res_matrix, thrust::complex<float> c0, const float del_y, const float del_x, const int threads) {
+__global__ void fill_matrix (int *res_matrix, const int w, const int h, thrust::complex<float> c0, const float del_y, const float del_x) {
     thrust::complex<float> del(0, 0);
-    const int w = res_matrix[0].size();
-    const int h = res_matrix.size();
     for (int i = 0; i < h; i += 1) {
         for (int j = 0; j < w; j += 1) {
             del.real(del_x * j);
             del.imag(del_y * i);
-            res_matrix[i][j] = get_inter(c0 + del);
+            res_matrix[i*w + j] = get_inter(c0 + del);
         }
     }
     return;
 }
 
-__host__ void prepare (thrust::device_vector<float> res_matrix, thrust::complex<float> c0, const float del_y, const float del_x, const int threads) {
-    std::cout << res_matrix.size() << std::endl;
-    std::cout << num_threads << std::endl;
-    std::cout << c0 << std::endl;
-    std::cout << del_y << std::endl;
-    std::cout << del_x << std::endl;
-
-    //fill_matrix<<<1, 1>>> (c_res_matrix, c_c0, c_del_y, c_del_x, c_threads);
-    return 0;
+__host__ void prepare (int *res_matrix, const int w, const int h, thrust::complex<float> c0, const float del_y, const float del_x, const int threads) {
+    int *d_res_matrix; 
+    int *d_w; 
+    int *d_h;
+    thrust::complex<float> *c_c0; 
+    float *d_del_y; 
+    float *d_del_x; 
+    cudaMalloc((void **) &d_res_matrix, sizeof(int)*w*h);
+    cudaMalloc((void **) &d_w, sizeof(int));
+    cudaMalloc((void **) &d_h, sizeof(int));
+    cudaMalloc((void **) &c_c0, sizeof(thrust::complex<float>));
+    cudaMalloc((void **) &d_del_y, sizeof(float));
+    cudaMalloc((void **) &d_del_x, sizeof(float));
+    cudaMemcpy(d_w, &w, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_h, &h, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(c_c0, &c0, sizeof(thrust::complex<float>), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_del_y, &del_y, sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_del_x, &del_x, sizeof(float), cudaMemcpyHostToDevice);
+    //fill_matrix<<<1, 1>>> (d_res_matrix, d_w, d_h, d_c0, d_del_y, d_del_x);
+    return;
 }
